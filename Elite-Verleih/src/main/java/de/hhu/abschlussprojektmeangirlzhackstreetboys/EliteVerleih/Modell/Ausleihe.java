d047bc100c3b07e59bc0e1cbcb0bd12ce832295b
neroc100 <neroc100@uni-duesseldorf.de>
2019-02-11 15:14:34 +0100

    Annotationen hinzugefuegt

--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/Modell/Ausleihe.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/Modell/Ausleihe.java
@@ -2,8 +2,19 @@ package de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.Modell;
 
 import java.util.Date;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Data
+@Entity
 class Ausleihe {
 
+    @Id
     private Long id;
     private Artikel artikel;
     private Date startdatum;
--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/Modell/Benutzer.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/Modell/Benutzer.java
@@ -1,7 +1,20 @@
 package de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.Modell;
+//import lombok.Data;
+import lombok.*;
 
-public class Benutzer {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Data
+@Entity
+class Benutzer {
+
+    @Id
     Long id;
     String username;
     String emailadresse;
