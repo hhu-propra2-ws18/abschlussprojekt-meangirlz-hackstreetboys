11fbc23b3066b226b33741f46b019d5d629155cf
Timo Stroick <timostroick@github.com>
2019-02-11 13:46:57 +0100

    Spring-Boot Anwendung hinzugefügt

---
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/EliteVerleihApplication.java
@@ -0,0 +1,14 @@
+package de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliteVerleihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliteVerleihApplication.class, args);
+	}
+
+}
+
