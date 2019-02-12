af06c7be7667f7a73a5de6ac563905553d6a6d2a
nirom100 <nina.romanow@t-online.de>
2019-02-12 10:15:27 +0100

    Artikel Modell verbessert

--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/Modell/Artikel.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/Modell/Artikel.java
@@ -5,11 +5,12 @@ import javax.persistence.Entity;
 
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
-public class Artikel {
+public class Artikel{
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
@@ -18,13 +19,45 @@ public class Artikel {
     private String artikelName;
     private String artikelBeschreibung;
     private Benutzer artikelBenutzer;
-    private int kaution;
-    private int tarif;
+    private int artikelKaution;
+    private int artikelTarif;
 
     private String artikelOrt;
-    private List<Ausleihe> ausleihList;
+    private List<Ausleihe> ausleiheList;
 
 
     public Artikel() {
     }
+
+    public Artikel(String artikelName, String artikelBeschreibung, Benutzer artikelBenutzer, int artikelKaution,
+                   int artikelTarif, String artikelOrt){
+        this.artikelName = artikelName;
+        this.artikelBeschreibung = artikelBeschreibung;
+        this.artikelBenutzer = artikelBenutzer;
+        this.artikelKaution = artikelKaution;
+        this.artikelTarif = artikelTarif;
+        this.artikelOrt = artikelOrt;
+        List<Ausleihe> ausl = new ArrayList<Ausleihe>();
+        this.ausleiheList = ausl;
+
+    }
+
+    public Artikel(String artikelName, String artikelBeschreibung, Benutzer artikelBenutzer,
+                   int artikelKaution, int artikelTarif, String artikelOrt, List<Ausleihe> ausleiheList){
+        this.artikelName = artikelName;
+        this.artikelBeschreibung = artikelBeschreibung;
+        this.artikelBenutzer = artikelBenutzer;
+        this.artikelKaution = artikelKaution;
+        this.artikelTarif = artikelTarif;
+        this.artikelOrt = artikelOrt;
+        this.ausleiheList = ausleiheList;
+
+    }
+
+    public void addAusleihe(Ausleihe ausleihe){
+        if(ausleiheList == null) ausleiheList = new ArrayList<Ausleihe>();
+        this.ausleiheList.add(ausleihe);
+    }
+
+
 }
