8dbbda194abd5dc6b7efb9fc5b1c5a2ce4b5e863
student <student@10.16.43.16>
2019-02-13 15:13:46 +0100

    PostMapping hinzugefuegt (lisch195&AndrejBri)

--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/controller/ProfilBearbeitenController.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/controller/ProfilBearbeitenController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.dataaccess.BenutzerRepository;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.modell.Benutzer;
@@ -13,6 +15,7 @@ import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.service.DataM
 public class ProfilBearbeitenController {
 	@Autowired
 	DataManager dataManager;
+	
     @GetMapping("/ProfilBearbeiten")
     public String ProfilBearbeitenAnzeigen(Long id, Model model){
     	if(id == null) {
@@ -21,5 +24,14 @@ public class ProfilBearbeitenController {
     	Benutzer benutzer = dataManager.getBenutzerById(id);
     	model.addAttribute("benutzer",benutzer);
 		return "ProfilBearbeiten";
+    }
+    @PostMapping("/ProfilBearbeiten")
+    public String ProfilBearbeitenSpeichern(Long id, @ModelAttribute Benutzer newBenutzer, Model model){
+    	if(id == null) {
+    		return "redirect:/";
+    	}
+    	Benutzer benutzer = dataManager.getBenutzerById(id);
+    	model.addAttribute("benutzer",benutzer);
+		return "redirect:/Uebersicht?id=" + benutzer.getBenutzerId();
     }
 }
