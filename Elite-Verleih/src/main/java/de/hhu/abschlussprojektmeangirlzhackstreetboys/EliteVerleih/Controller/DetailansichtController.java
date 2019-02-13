63a7297c581f5e93a2afc82443bf8124d8832bb8
neroc100 <neroc100@uni-duesseldorf.de>
2019-02-13 10:16:34 +0100

    Detailansicht fuer null Objekte angepasst + seperater branch (nirom100)

--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/Controller/DetailansichtController.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/Controller/DetailansichtController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DetailansichtController {
@@ -14,9 +15,9 @@ public class DetailansichtController {
     ArtikelRepository artikel;
 
 
-    @GetMapping("/Detailansicht")
-    public String DetailansichtAnzeigen(Model model){
-        model.addAttribute("artikel", artikel.findAll() );
+    @GetMapping("/Detailansicht/{id}")
+    public String DetailansichtAnzeigen(@PathVariable long id, Model model){
+        model.addAttribute("artikel", artikel.findArtikelByArtikelId(id) );
         return "Detailansicht";
     }
 
