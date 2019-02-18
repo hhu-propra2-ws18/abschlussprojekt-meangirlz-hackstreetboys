07334b6cb92f56115704dc31676babd6d555c329
lissaschwarz@yahoo.de <Uniteeminem2>
2019-02-18 10:27:47 +0100

    Konfliktloesestelle für Admin erstellt (lisch195&nirom100)

---
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/controller/KonfliktController.java
@@ -0,0 +1,41 @@
+package de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.controller;
+
+import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.modell.Ausleihe;
+import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.modell.Benutzer;
+import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.service.ArtikelManager;
+import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.service.AusleiheManager;
+import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.service.BenutzerManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class KonfliktController {
+
+    @Autowired
+    BenutzerManager benutzerManager;
+
+    @Autowired
+    AusleiheManager ausleiheManager;
+
+    @Autowired
+    ArtikelManager artikelManager;
+
+    @GetMapping("/Konfliktloesung")
+    public String ProfilAnzeigen(Long id, Model model){
+        if(id != 1) {
+            return "redirect:/";
+        }
+
+        //alle Benutzer
+        //List<Ausleihe> ausleihe = ausleiheManager.
+
+        Benutzer benutzer = benutzerManager.getBenutzerById(id);
+        model.addAttribute("benutzer",benutzer);
+
+        model.addAttribute("konflikt", benutzerManager.sucheAnfragen(benutzer, new String("KONFLIKT")));
+
+        return "Konfliktloesung";
+    }
+}
