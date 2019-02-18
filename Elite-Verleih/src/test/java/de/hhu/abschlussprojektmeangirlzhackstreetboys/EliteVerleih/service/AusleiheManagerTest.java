2b261e5890b74902b0a635cbcc08d12b6118f5b7
exalw <exalw@yahoo.de>
2019-02-18 14:54:38 +0100

    Tests zum korrekten Mapping der Datenbank eingefügt. (AndrejBri)

--- Elite-Verleih/src/test/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/service/AusleiheManagerTest.java
+++ Elite-Verleih/src/test/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/service/AusleiheManagerTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.assertj.AssertableReactiveWebApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -62,7 +63,7 @@ public class AusleiheManagerTest {
         b0.setBenutzerEmail("test@yahoo");
         b0.setBenutzerName("test");
         b0.setArtikel(new ArrayList<Artikel>());
-        benutzerRepo.save(b0);
+        b0 = benutzerRepo.save(b0);
         Artikel a0 = new Artikel();
         a0.setArtikelBeschreibung("beschreibung");
         a0.setArtikelKaution(3);
@@ -70,11 +71,78 @@ public class AusleiheManagerTest {
         a0.setArtikelOrt("Werkstatt");
         a0.setArtikelTarif(1);
         a0.setBenutzer(b0);
-        artikelRepo.save(a0);
+        a0 = artikelRepo.save(a0);
         Date sD0 = new Date(2019, 5, 8);
         Date eD0 = new Date(2019, 5, 10);
-        ausleiheM.erstelleAusleihe(benutzerRepo.findBenutzerByBenutzerName("test").get().getBenutzerId(),
-                artikelRepo.findArtikelByArtikelName("Hammer").get().getArtikelId(),sD0,eD0);
-        Assertions.assertThat(artikelRepo.findArtikelByArtikelName("Hammer").get().getAusgeliehen().size()==1);
+        Ausleihe ausleihe = ausleiheM.erstelleAusleihe(b0.getBenutzerId(), a0.getArtikelId(), sD0, eD0);
+        Assertions.assertThat(a0.getAusgeliehen().size()==1);
+    }
+
+    @Rollback
+    @Test
+    public void deleteAusleihe_RichtigeMappings_AusleiheinBesitzer(){
+        Benutzer b0 = new Benutzer();
+        b0.setBenutzerEmail("test@yahoo");
+        b0.setBenutzerName("Ausleiher");
+        b0.setAusgeliehen(new ArrayList<Ausleihe>());
+        b0 = benutzerRepo.save(b0);
+        Benutzer b1 = new Benutzer();
+        b1.setBenutzerEmail("test@yahoo");
+        b1.setBenutzerName("Besitzer");
+        b1.setArtikel(new ArrayList<Artikel>());
+        b1 = benutzerRepo.save(b1);
+        Artikel a0 = new Artikel();
+        a0.setArtikelBeschreibung("beschreibung");
+        a0.setArtikelKaution(3);
+        a0.setArtikelName("Hammer");
+        a0.setArtikelOrt("Werkstatt");
+        a0.setArtikelTarif(1);
+        a0.setBenutzer(b1);
+        a0 = artikelRepo.save(a0);
+        b1.getArtikel().add(a0);
+        benutzerRepo.save(b1);
+        Date sD0 = new Date(2019, 5, 8);
+        Date eD0 = new Date(2019, 5, 10);
+        Ausleihe ausleihe = ausleiheM.erstelleAusleihe(b0.getBenutzerId(), a0.getArtikelId(), sD0, eD0);
+
+        ausleiheM.loescheAusleihe(ausleihe.getAusleihId());
+        for(Artikel a : benutzerRepo.findBenutzerByBenutzerId(b1.getBenutzerId()).getArtikel()){
+            if(a.getArtikelId()==a0.getArtikelId()){
+                System.err.println("if erreicht!!!");
+                Assertions.assertThat(!a.getAusgeliehen().contains(ausleihe));
+            }
+        }
+    }
+
+    @Rollback
+    @Test
+    public void deleteAusleihe_RichtigeMappings_AusleiheinAusleiher(){
+        Benutzer b0 = new Benutzer();
+        b0.setBenutzerEmail("test@yahoo");
+        b0.setBenutzerName("Ausleiher");
+        b0.setAusgeliehen(new ArrayList<Ausleihe>());
+        b0 = benutzerRepo.save(b0);
+        Benutzer b1 = new Benutzer();
+        b1.setBenutzerEmail("test@yahoo");
+        b1.setBenutzerName("Besitzer");
+        b1.setArtikel(new ArrayList<Artikel>());
+        b1 = benutzerRepo.save(b1);
+        Artikel a0 = new Artikel();
+        a0.setArtikelBeschreibung("beschreibung");
+        a0.setArtikelKaution(3);
+        a0.setArtikelName("Hammer");
+        a0.setArtikelOrt("Werkstatt");
+        a0.setArtikelTarif(1);
+        a0.setBenutzer(b1);
+        a0 = artikelRepo.save(a0);
+        b1.getArtikel().add(a0);
+        benutzerRepo.save(b1);
+        Date sD0 = new Date(2019, 5, 8);
+        Date eD0 = new Date(2019, 5, 10);
+        Ausleihe ausleihe = ausleiheM.erstelleAusleihe(b0.getBenutzerId(), a0.getArtikelId(), sD0, eD0);
+
+        ausleiheM.loescheAusleihe(ausleihe.getAusleihId());
+        Assertions.assertThat(b0.getAusgeliehen().contains(ausleihe));
     }
 }
+
