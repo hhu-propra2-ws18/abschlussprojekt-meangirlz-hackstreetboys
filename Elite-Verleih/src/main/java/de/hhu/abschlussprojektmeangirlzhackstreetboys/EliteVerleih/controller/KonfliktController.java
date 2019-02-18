2782f6fffdbcb735e78d97e784d401a92c34dcdc
lissaschwarz@yahoo.de <Uniteeminem2>
2019-02-18 13:25:45 +0100

    Button für Konflikt wurde aktualisiert und aendert nun den Status (nirom100)

--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/controller/KonfliktController.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/controller/KonfliktController.java
@@ -2,6 +2,7 @@ package de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.controller;
 
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.modell.Ausleihe;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.modell.Benutzer;
+import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.modell.Status;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.service.ArtikelManager;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.service.AusleiheManager;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.service.BenutzerManager;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 @Controller
@@ -32,5 +36,34 @@ public class KonfliktController {
         List<Ausleihe> ausleihe = ausleiheManager.getAllAusleihe();
         model.addAttribute("konflikt", ausleiheManager.getKonflike(ausleihe));
         return "Konfliktloesung";
+    }
+
+    @PostMapping("/Konfliktloesung")
+    public String profilAnzeigen(@RequestParam(value= "submitButton") String name,
+                                 @ModelAttribute Ausleihe anfrage, Long ausleihId) {
+
+        Ausleihe ausleihe = ausleiheManager.getAusleiheById(ausleihId);
+        Benutzer benutzer;
+
+        System.out.println("Test:"+ ausleihId);
+
+        if (name.equals("Buchung Verleihender")) {
+            System.out.println("Test1 ");
+            benutzer = ausleihe.getArtikel().getBenutzer();
+            // Kaution wird an Verleihenden gezahlt
+        }
+
+        if (name.equals("Buchung Ausleihender")) {
+            benutzer = ausleihe.getBenutzer();
+            // Kaution wird an Ausleihenden zurückgezahlt
+        }
+
+        if (name.equals("Konflikt beenden")) {
+            ausleiheManager.setzeSatusAusleihe(ausleihe, "BEENDET");
+        }
+
+
+        return "redirect:/Konfliktloesung?id=1";
+
     }
 }
