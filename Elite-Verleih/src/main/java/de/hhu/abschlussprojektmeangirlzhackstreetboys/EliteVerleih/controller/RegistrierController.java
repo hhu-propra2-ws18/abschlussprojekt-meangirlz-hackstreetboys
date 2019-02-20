eb3aa40d8dba26d130271085c1e37f4d21778d22
Timo Stroick <timostroick@github.com>
2019-02-20 12:44:49 +0100

    DataInitializer angepasst und Konfliktloesung fuer User blockiert (neroc100)

--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/controller/RegistrierController.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/EliteVerleih/controller/RegistrierController.java
@@ -3,20 +3,14 @@ package de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.controller;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.modell.Benutzer;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.EliteVerleih.service.BenutzerManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.nio.file.attribute.UserPrincipal;
 
 @Controller
 public class RegistrierController {
@@ -29,7 +23,6 @@ public class RegistrierController {
         model.addAttribute("benutzer", new Benutzer());
         return "Registrierung";
     }
-
     @RequestMapping(value = "/registrieren", method = RequestMethod.POST)
     public String registerBenutzer
             (@ModelAttribute @Valid Benutzer benutzer,
@@ -48,7 +41,4 @@ public class RegistrierController {
         request.login(registered.getBenutzerName(), registered.getBenutzerPasswort());
         return "redirect:/Uebersicht";
     }
-
-
-
 }
