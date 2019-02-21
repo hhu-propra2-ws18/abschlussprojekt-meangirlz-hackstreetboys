9e19cb065d1aa9d40cacb2b643a3d2b5afb27b5a
neroc100 <neroc100@uni-duesseldorf.de>
2019-02-21 11:54:19 +0100

    Buchung nach Bestaetigung entfernt, Navbar angepasst (paatty32)

--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/eliteverleih/controller/KonfliktController.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/eliteverleih/controller/KonfliktController.java
@@ -32,13 +32,27 @@ public class KonfliktController {
     @Autowired
     ArtikelManager artikelManager;
 
+    /**
+     * Zeigt Konfliktseite an, laedt Attribute in HTML.
+     * @param model  Datencontainer fuer View.
+     * @param account aktueller Benutzer.
+     * @return Mapping auf Konfliktloesung.
+     */
     @GetMapping("/Konfliktloesung")
     public String konfliktloesungAnzeigen(Model model, Principal account) {
         List<Ausleihe> ausleihe = ausleiheManager.getAllAusleihe();
         model.addAttribute("konflikt", ausleiheManager.getKonflike(ausleihe));
+        model.addAttribute("benutzer", benutzerManager.findBenutzerByName(account.getName()));
         return "Konfliktloesung";
     }
 
+    /**
+     * Bearbeitet Kaution bei einem Konflikt, loescht dann Buchung.
+     * @param name ButtonName.
+     * @param anfrage zu verwaltende Anfrage.
+     * @param ausleihId Id der Anfrage.
+     * @return Weiterleitung auf Konfliktloesung
+     */
     @PostMapping("/Konfliktloesung")
     public String konfliktLoesen(@RequestParam(value = "submitButton") String name,
                                  @ModelAttribute Ausleihe anfrage, Long ausleihId) {
@@ -49,11 +63,13 @@ public class KonfliktController {
         if (name.equals("Buchung Verleihender")) {
             sync.getAccount(benutzer.getBenutzerName());
             sync.kautionEinziehen(ausleihe.getBenutzer().getBenutzerName(), ausleihe.getReservationsId());
+            ausleiheManager.bearbeiteAusleihe(ausleihId, Status.BEENDET);
         }
 
         if (name.equals("Buchung Ausleihender")) {
             sync.getAccount(benutzer.getBenutzerName());
             sync.kautionFreigeben(ausleihe.getBenutzer().getBenutzerName(), ausleihe.getReservationsId());
+            ausleiheManager.bearbeiteAusleihe(ausleihId, Status.BEENDET);
         }
 
         if (name.equals("Konflikt beenden")) {
