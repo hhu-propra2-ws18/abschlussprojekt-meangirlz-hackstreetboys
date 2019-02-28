fb632396e6e71f11243845ae985ae0cc0e0cbf06
nirom100 <nina.romanow@t-online.de>
2019-02-28 19:06:07 +0100

    Transaktionen View angepasst + Link zu Transaktionen in Navbar (lisch195)

--- Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/eliteverleih/controller/TransaktionenController.java
+++ Elite-Verleih/src/main/java/de/hhu/abschlussprojektmeangirlzhackstreetboys/eliteverleih/controller/TransaktionenController.java
@@ -3,27 +3,35 @@ package de.hhu.abschlussprojektmeangirlzhackstreetboys.eliteverleih.controller;
 
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.eliteverleih.dto.AccountDto;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.eliteverleih.modell.Benutzer;
+import de.hhu.abschlussprojektmeangirlzhackstreetboys.eliteverleih.modell.Transaktion;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.eliteverleih.service.BenutzerManager;
 import de.hhu.abschlussprojektmeangirlzhackstreetboys.eliteverleih.service.PropayManager;
+import de.hhu.abschlussprojektmeangirlzhackstreetboys.eliteverleih.service.TransaktionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.validation.constraints.Max;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class TransaktionenController {
 
     BenutzerManager benutzerManager;
 
+    TransaktionManager transaktionManager;
+
     PropayManager propayManager;
 
 
     @Autowired
     public TransaktionenController(BenutzerManager benutzerManager,
+                                   TransaktionManager transaktionManager,
                                    PropayManager propayManager){
         this.benutzerManager = benutzerManager;
+        this.transaktionManager =transaktionManager;
         this.propayManager = propayManager;
     }
 
@@ -31,8 +39,8 @@ public class TransaktionenController {
     /**
      * Zeigt die Transaktionenseite an
      *
-     * @param model     Datencontainer für die View
-     * @param account   Account des aktuellen Users
+     * @param model         Datencontainer für die View
+     * @param account       Account des aktuellen Users
      * @return "Transaktionen"
      */
 
@@ -40,6 +48,9 @@ public class TransaktionenController {
     public String transaktionenAnzeigen(Model model, Principal account){
         Benutzer benutzer = benutzerManager.findBenutzerByName(account.getName());
         model.addAttribute("benutzer", benutzer);
+
+        List<Transaktion> transaktion = transaktionManager.getAllTransaktion();
+        model.addAttribute("transaktion", transaktion);
 
         AccountDto account1 = propayManager.getAccount(benutzer.getBenutzerName());
         if (account1 == null) {
